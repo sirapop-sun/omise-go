ef3e67701cfcd4587a420c6e12f023d16a3fd4f8
sirapop_kon <sirapop.kon@truedigital.com>
2019-12-02 17:07:11 +0700

    [edit] lookup endpoint from env

--- internal/endpoint.go
+++ internal/endpoint.go
@@ -1,8 +1,18 @@
 package internal
 
+import "os"
+
 type Endpoint string
 
-const (
-	API   Endpoint = "https://api.omise.co"
-	Vault          = "https://vault.omise.co"
+var (
+	API   = getEnv("OMISE_API", "https://api.omise.co")
+	Vault = getEnv("OMISE_VAULT", "https://vault.omise.co")
 )
+
+func getEnv(key, fallback string) Endpoint {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
+	}
+	return Endpoint(value)
+}
